c8c15525db05f89a213a19d7b514697a7b18e650
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2022-12-01 21:35:41 +0200

    styles :tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        primary:{
+          500:"#FF2727",
+          300:"#FFE4E4",
+        },
+        secondary:"#F9F9F9",
+        txt:{
+          primary:"#000",
+          secondary:"#CBCBCB",
+        }
+      },
+      fontWeight:{
+        "bold": "700",
+      }
+    },
   },
   plugins: [],
 }
